56ce33dbd4b77a3feeaa8e2ff1adecebc0aabd6c
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-09-26 16:57:29 +0000

    lookup_people,js searches through database famous_people and searches for them by first or last name and returns all information about them

---
+++ lookup_people.js
@@ -0,0 +1,32 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+//take process.argv[2] as the input text to be searched
+
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  const searchTerm = process.argv[2]
+  client.query("SELECT * FROM famous_people WHERE last_name = $1 OR first_name = $1", [searchTerm], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log("Searching,,,")
+    console.log(`Found ${result.rows.length} person(s) by the name ${searchTerm}`)
+    result.rows.forEach( function(person) {
+      console.log(`- ${person.id}: ${person.first_name} ${person.last_name}, born ${person.birthdate}`)
+    });
+    client.end();
+  });
+});
